024befb20c64ebf934f6747a0dfc4e8180bd7801
Dan Allen <dallen@redhat.com>
2013-07-01 15:11:59 -0600

    rework continuous integration build to use gem install strategy

---
+++ test-asciidoctor-master.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -O asciidoctor-master.zip https://github.com/asciidoctor/asciidoctor/archive/master.zip
+unzip asciidoctor-master.zip
+cp install-asciidoctor-gem.pom asciidoctor-master/pom.xml
+cd asciidoctor-master
+ASCIIDOCTOR_VERSION=`grep 's\.version' asciidoctor.gemspec | sed "s/.*'\(.*\)'.*/\1/"`
+sed -i "s;<version></version>;<version>$ASCIIDOCTOR_VERSION</version>;" pom.xml
+mvn install
+cd ..
+rm -rf asciidoctor-master*
+mvn test -Dasciidoctor.version=$ASCIIDOCTOR_VERSION
