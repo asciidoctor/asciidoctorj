6a2f98658362fe5006fa5a12ca8dddd887668e25
alex <asotobu@gmail.com>
2014-02-14 20:46:27 +0100

    resolves issue #140 by commenting one recursive line which it was not necessary, but makes when section has no content to return null.

--- src/test/java/org/asciidoctor/WhenStructuredDocumentIsRequired.java
+++ src/test/java/org/asciidoctor/WhenStructuredDocumentIsRequired.java
@@ -27,13 +27,26 @@ public class WhenStructuredDocumentIsRequired {
 	private Asciidoctor asciidoctor = JRubyAsciidoctor.create();
 
 	@Test
+	public void empty_parent_title_makes_subsection_be_null_when_is_parsed() {
+
+		String s = "= My page\n" + "\n" + "== Totally ignored header\n" + "\n"
+				+ "What does it mean?\n";
+
+		StructuredDocument structuredDocument = asciidoctor
+				.readDocumentStructure(s, new java.util.HashMap());
+		
+		List<ContentPart> parts = structuredDocument.getParts();
+
+	}
+
+	@Test
 	public void structural_content_should_be_retrieved_from_file() {
 
-		Map<String,Object> parameters = new HashMap<String, Object>();
+		Map<String, Object> parameters = new HashMap<String, Object>();
 		parameters.put(Asciidoctor.STRUCTURE_MAX_LEVEL, 2);
 		StructuredDocument document = asciidoctor.readDocumentStructure(
-				new File("target/test-classes/documentblocks.asciidoc"),parameters
-				);
+				new File("target/test-classes/documentblocks.asciidoc"),
+				parameters);
 
 		DocumentHeader header = document.getHeader();
 
@@ -44,7 +57,7 @@ public class WhenStructuredDocumentIsRequired {
 		assertThat((String) attributes.get("revdate"), is("2013-05-20"));
 		assertThat((String) attributes.get("revnumber"), is("1.0"));
 		assertThat((String) attributes.get("revremark"), is("First draft"));
-		//attributes should be incasesensitive
+		// attributes should be incasesensitive
 		assertThat((String) attributes.get("tags"), is("[document, example]"));
 		assertThat((String) attributes.get("Tags"), is("[document, example]"));
 		assertThat((String) attributes.get("author"), is("Doc Writer"));
@@ -58,33 +71,33 @@ public class WhenStructuredDocumentIsRequired {
 
 		assertThat(parts.get(0).getContext(), is("section"));
 		assertThat(parts.get(0).getTitle(), is("Abstract"));
-		
+
 		assertThat(parts.get(0).getParts(), hasSize(1));
 		assertThat(parts.get(0).getParts().get(0).getStyle(), is("abstract"));
 		assertThat(parts.get(0).getParts().get(0).getContext(), is("open"));
-		
+
 		assertThat(parts.get(1).getTitle(), is("First Section"));
 		assertThat(parts.get(1).getId(), is("_first_section"));
 		assertThat(parts.get(1).getContext(), is("section"));
 		assertThat(parts.get(1).getParts(), hasSize(5));
 
-		
 		assertThat(parts.get(1).getParts().get(1).getId(), is("blockid"));
 		assertThat(parts.get(1).getParts().get(1).getStyle(), is("quote"));
-		assertThat((String)parts.get(1).getParts().get(1).getAttributes().get("attribution"), is("Abraham Lincoln"));
-
+		assertThat(
+				(String) parts.get(1).getParts().get(1).getAttributes()
+						.get("attribution"), is("Abraham Lincoln"));
 
 		assertThat(parts.get(1).getParts().get(2).getRole(), is("feature-list"));
 
 		assertThat(parts.get(2).getTitle(), is("Second Section"));
 		assertThat(parts.get(2).getParts(), hasSize(2));
-		
+
 		assertThat(parts.get(2).getParts().get(0).getContext(), is("image"));
 		assertThat(parts.get(2).getParts().get(1).getContext(), is("image"));
 	}
-	
+
 	@Test
-	public void some_real_content(){
+	public void some_real_content() {
 		StructuredDocument document = asciidoctor.readDocumentStructure(
 				new File("target/test-classes/contentstructure.asciidoc"),
 				new HashMap<String, Object>());
@@ -101,18 +114,21 @@ public class WhenStructuredDocumentIsRequired {
 		assertThat(parts, notNullValue());
 		assertThat(parts, hasSize(4));
 
-		
-		
-		assertThat(document.getPartByStyle("literal").getContent(), is("TODO: This is description"));
-		
+		assertThat(document.getPartByStyle("literal").getContent(),
+				is("TODO: This is description"));
+
 		List<ContentPart> images = document.getPartsByContext("image");
 		assertThat(images, hasSize(2));
-		for (int i = 0; i<2; i++) {
-			assertThat((String) images.get(i).getAttributes().get("target"), is("src/some image "+(i+1)+".JPG"));
-			assertThat((String) images.get(i).getAttributes().get("alt"), is("TODO title"+(i+1)));
-			assertThat((String) images.get(i).getAttributes().get("link"), is("link"+(i+1)+".html"));
+		for (int i = 0; i < 2; i++) {
+			assertThat((String) images.get(i).getAttributes().get("target"),
+					is("src/some image " + (i + 1) + ".JPG"));
+			assertThat((String) images.get(i).getAttributes().get("alt"),
+					is("TODO title" + (i + 1)));
+			assertThat((String) images.get(i).getAttributes().get("link"),
+					is("link" + (i + 1) + ".html"));
 		}
-		assertThat(document.getPartByStyle("Open").getContent(), startsWith("<div class=\"paragraph text-center\">"));
+		assertThat(document.getPartByStyle("Open").getContent(),
+				startsWith("<div class=\"paragraph text-center\">"));
 	}
 
 	@Test
@@ -146,16 +162,15 @@ public class WhenStructuredDocumentIsRequired {
 
 		assertThat(parts, notNullValue());
 		assertThat(parts, hasSize(1));
-		assertThat(parts.get(0)
-				.getContent(), is("Simple single paragraph"));
+		assertThat(parts.get(0).getContent(), is("Simple single paragraph"));
 
 	}
-	
+
 	@Test
 	public void no_parts_should_be_retrieved_from_empty_string() {
 
-		StructuredDocument document = asciidoctor.readDocumentStructure(
-				"", new HashMap<String, Object>());
+		StructuredDocument document = asciidoctor.readDocumentStructure("",
+				new HashMap<String, Object>());
 
 		DocumentHeader header = document.getHeader();
 
@@ -166,5 +181,5 @@ public class WhenStructuredDocumentIsRequired {
 		assertThat(parts, notNullValue());
 		assertThat(parts, hasSize(0));
 	}
-	
+
 }
