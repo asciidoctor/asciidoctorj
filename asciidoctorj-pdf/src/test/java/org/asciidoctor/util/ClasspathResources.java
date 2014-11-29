a56733ac9a67e4f09a49a662dd915a674316b6d8
Dan Allen <dan.j.allen@gmail.com>
2014-11-29 04:31:45 -0700

    resolves #194 migrate project to multi-module Gradle build
    - add multi-module Gradle build
    - remove Maven build script (pom.xml)
    - setup Gradle wrapper
    - relocate core to asciidoctorj-core subproject
    - setup asciidoctorj-epub3, asciidoctorj-pdf and asciidoctorj-distribution subprojects
    - move bundling of asciidoctorj-epub3 into separate jar
    - define version and properName properties for each artifact in gradle.properties
    - use JRuby Gradle plugin to manage gems
    - bundle only essential sources from gems; copy to output folder of main source set
    - configure Gradle to generate Eclipse and IDEA project files
    - create distribution zip for AsciidoctorJ (bundles core, epub3 and pdf)
    - use Gradle to generate start scripts for distribution
    - optimize JVM flags for start scripts in distribution
    - add additional information to manifest of artifacts
    - configure install to local Maven repository
    - configure signing (GPG) plugin
    - configure upload to Bintray (creates package & version; includes distribution zip)
    - update script to test against Asciidoctor upstream (using Maven to install gem)
    - activate and fix broken iconfont test
    - autoload asciidoctor-pdf gem when backend is set to pdf

---
+++ asciidoctorj-pdf/src/test/java/org/asciidoctor/util/ClasspathResources.java
@@ -0,0 +1,28 @@
+package org.asciidoctor.util;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+/**
+ * JUnit TestRule to handle classpath files.
+ * 
+ *  Delegates to {@link ClasspathHelper}
+ */
+public class ClasspathResources extends ClasspathHelper implements TestRule {
+
+    protected void before(Class<?> clazz) throws Throwable {
+        super.setClassloader(clazz);
+    }
+
+    @Override
+    public Statement apply(final Statement base, final Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                before(description.getTestClass());
+                base.evaluate();
+            }
+        };
+    }
+}
