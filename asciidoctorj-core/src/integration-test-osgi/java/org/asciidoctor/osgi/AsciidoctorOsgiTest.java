9dea1e9991d35e541477acb00f92aa7713160de2
Tobias Roeser <le.petit.fou@web.de>
2015-07-17 10:26:20 +0200

    Slightly cleaned up and extended the Arquillian OSGi test

--- asciidoctorj-core/src/integration-test-osgi/java/org/asciidoctor/osgi/AsciidoctorOsgiTest.java
+++ asciidoctorj-core/src/integration-test-osgi/java/org/asciidoctor/osgi/AsciidoctorOsgiTest.java
@@ -1,66 +1,54 @@
 package org.asciidoctor.osgi;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
 import org.asciidoctor.Asciidoctor;
 import org.asciidoctor.OptionsBuilder;
 import org.asciidoctor.SafeMode;
-import org.jboss.arquillian.container.test.api.OperateOnDeployment;
-import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
-import org.junit.Test;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
-
-import java.io.File;
-import java.util.Arrays;
-
-import static org.junit.Assert.assertTrue;
 
 @RunWith(Arquillian.class)
 public class AsciidoctorOsgiTest {
 
-    @Deployment(name = "AsciidoctorJ", order = 1)
+    @Deployment(name = "AsciidoctorJ")
     public static JavaArchive createAsciidoctorDeployment() {
         return ShrinkWrap.createFromZipFile(JavaArchive.class, new File(System.getProperty("asciidoctorJarName")));
     }
 
-    @Deployment(name = "Test", order = 2)
-    public static JavaArchive createTestDeployment() {
-        return ShrinkWrap.create(JavaArchive.class, "test.jar")
-            .setManifest(new ClassLoaderAsset("TestManifest.mf"));
-    }
-
-    @ArquillianResource
-    private BundleContext ctx;
-
-    @OperateOnDeployment("Test")
+    @OperateOnDeployment("AsciidoctorJ")
     @Test
-    public void shouldRenderSimpleDocumentToHtml() throws Exception {
-        Bundle bundle = getBundle("org.asciidoctor");
-        bundle.start();
+    public void shouldRenderSimpleDocumentToHtml(@ArquillianResource final BundleContext ctx, @ArquillianResource final Bundle asciidoctorBundle)
+            throws Exception {
 
-        ServiceReference serviceref = ctx.getServiceReference("org.asciidoctor.Asciidoctor");
-        Asciidoctor asciidoctor = (Asciidoctor) ctx.getService(serviceref);
+        Assert.assertNull(ctx.getServiceReference(Asciidoctor.class.getName()));
 
-        String result = asciidoctor.convert("Hello World", OptionsBuilder.options().safe(SafeMode.UNSAFE).headerFooter(false).toFile(false));
+        asciidoctorBundle.start();
+
+        final ServiceReference<?> reference = ctx.getServiceReference(Asciidoctor.class.getName());
+        final Asciidoctor asciidoctor = (Asciidoctor) ctx.getService(reference);
+        final String result = asciidoctor.convert("Hello World",
+                OptionsBuilder.options().safe(SafeMode.UNSAFE).headerFooter(false).toFile(false));
         System.out.println("RESULT: " + result);
-        assertTrue("Result does not have expected content: \n" + result, result.contains("Hello World"));
+        assertTrue("Result does not have expected content: \n" + result,
+                result.contains("Hello World"));
 
-    }
+        asciidoctorBundle.stop();
 
-    public Bundle getBundle(String bundleName) {
-        for (Bundle bundle: ctx.getBundles()) {
-            if (bundle.getSymbolicName().equals(bundleName)) {
-                return bundle;
-            }
-        }
-        return null;
+        Assert.assertNull(ctx.getServiceReference(Asciidoctor.class.getName()));
+
     }
 
 }
