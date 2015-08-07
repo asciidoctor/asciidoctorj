b7d73fddcff4e84a928a93adeb29142e7beda462
Robert Panzer <robert.panzer@me.com>
2015-08-07 11:35:13 +0200

    Add constructor to AsciidoctorCoreException that takes a String and a Throwable so that Asciidoctor Core can correctly use it to wrap caught exceptions

--- asciidoctorj-core/src/main/java/org/asciidoctor/internal/AsciidoctorCoreException.java
+++ asciidoctorj-core/src/main/java/org/asciidoctor/internal/AsciidoctorCoreException.java
@@ -5,4 +5,9 @@ public class AsciidoctorCoreException extends RuntimeException {
     public AsciidoctorCoreException(Throwable e) {
         super(e);
     }
+
+    public AsciidoctorCoreException(String message, Throwable e) {
+        super(message, e);
+    }
+
 }
