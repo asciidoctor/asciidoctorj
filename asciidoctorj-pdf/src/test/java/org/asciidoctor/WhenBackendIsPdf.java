38fe8ed944455b2eb8683022909e0722a3dcaa49
abelsromero <abelromero@gmail.com>
2015-08-09 00:24:23 +0200

    Integrated PDBBox to validate PDF generation in asciidoctorj-pdf

--- asciidoctorj-pdf/src/test/java/org/asciidoctor/WhenBackendIsPdf.java
+++ asciidoctorj-pdf/src/test/java/org/asciidoctor/WhenBackendIsPdf.java
@@ -1,21 +1,26 @@
 package org.asciidoctor;
 
-import static org.asciidoctor.OptionsBuilder.options;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
-import java.io.File;
-
-import org.asciidoctor.Asciidoctor;
 import org.asciidoctor.arquillian.api.Unshared;
 import org.asciidoctor.util.ClasspathResources;
+import org.asciidoctor.util.RougeColors;
+import org.asciidoctor.util.pdf.ColorsProcessor;
+import org.asciidoctor.util.pdf.ImageProcessor;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jruby.runtime.builtin.IRubyObject;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static org.asciidoctor.OptionsBuilder.options;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(Arquillian.class)
 public class WhenBackendIsPdf {
@@ -29,24 +34,58 @@ public class WhenBackendIsPdf {
     private ClasspathResources classpath;
 
     @Test
-    public void pdf_should_be_rendered_for_pdf_backend() {
-        File inputFile = classpath.getResource("sample.adoc");
-        File outputFile1 = new File(inputFile.getParentFile(), "sample.pdf");
-        File outputFile2 = new File(inputFile.getParentFile(), "sample.pdfmarks");
-        asciidoctor.convertFile(inputFile, options().backend("pdf").safe(SafeMode.UNSAFE).get());
-        assertThat(outputFile1.exists(), is(true));
-        assertThat(outputFile2.exists(), is(true));
-        outputFile1.delete();
-        outputFile2.delete();
-    }
-
-    @Test
     public void pdf_should_be_rendered_to_object_for_pdf_backend() {
         // The asciidoctor-pdf backend returns the converter itself on convert.
         // If the result should be written to a file the write method will convert to a PDF stream
         // Therefore, if the result should not be written to a file the PDF converter should be returned.
         IRubyObject o = asciidoctor.convert(DOCUMENT, options().backend("pdf").get(), IRubyObject.class);
         assertThat(o.getMetaClass().getRealClass().getName(), is("Asciidoctor::Pdf::Converter"));
+    }
+
+    @Test
+    public void pdf_should_include_images() throws IOException {
+        String filename = "image-sample";
+        File inputFile = this.classpath.getResource(filename + ".adoc");
+        File outputFile1 = new File(inputFile.getParentFile(), filename + ".pdf");
+        File outputFile2 = new File(inputFile.getParentFile(), filename + ".pdfmarks");
+
+        asciidoctor.convertFile(inputFile, options().backend("pdf").safe(SafeMode.UNSAFE).get());
+
+        assertThat(outputFile1.exists(), is(true));
+        assertThat(outputFile2.exists(), is(true));
+        ImageProcessor imageProcessor = new ImageProcessor();
+        imageProcessor.parse(outputFile1.getAbsolutePath());
+        List images = imageProcessor.getImages();
+        assertThat(images.size(), is(2));
+
+        outputFile1.delete();
+        outputFile2.delete();
+    }
+
+    @Test
+    public void pdf_source_code_should_be_highlighted() throws IOException {
+        String filename = "code-sample";
+        File inputFile = this.classpath.getResource(filename + ".adoc");
+        File outputFile1 = new File(inputFile.getParentFile(), filename + ".pdf");
+        File outputFile2 = new File(inputFile.getParentFile(), filename + ".pdfmarks");
+
+        asciidoctor.convertFile(inputFile, options().backend("pdf").safe(SafeMode.UNSAFE).get());
+
+        assertThat(outputFile1.exists(), is(true));
+        assertThat(outputFile2.exists(), is(true));
+        ColorsProcessor colorsProcessor = new ColorsProcessor("program", "System.out.println", "printHello", "HelloWorld", "<body>", "else", "Math.sqrt" );
+        colorsProcessor.parse(outputFile1.getAbsolutePath());
+        Map<String, List<Color>> colors = colorsProcessor.getColors();
+        assertThat(colors.get("program").get(0), equalTo(RougeColors.GREY));
+        assertThat(colors.get("System.out.println").get(0), equalTo(RougeColors.LIGHT_BLUE));
+        assertThat(colors.get("printHello").get(0), equalTo(RougeColors.DARK_BLUE));
+        assertThat(colors.get("HelloWorld").get(0), equalTo(RougeColors.PINK));
+        assertThat(colors.get("<body>").get(0), equalTo(RougeColors.PINK));
+        assertThat(colors.get("else").get(0), equalTo(RougeColors.GREEN));
+        assertThat(colors.get("Math.sqrt").get(0), equalTo(RougeColors.LIGHT_BLUE));
+
+        outputFile1.delete();
+        outputFile2.delete();
     }
 
 }
---
+++ asciidoctorj-pdf/src/test/java/org/asciidoctor/util/RougeColors.java
@@ -0,0 +1,22 @@
+package org.asciidoctor.util;
+
+import java.awt.*;
+
+/**
+ * Constants for the colors used by Rouge (http://rouge.jneen.net/)
+ *
+ * @author abelsromero
+ */
+public class RougeColors {
+
+    public static final Color GREEN = new Color(0,136,0);
+
+    public static final Color LIGHT_BLUE = new Color(51,102,153);
+
+    public static final Color DARK_BLUE = new Color(0,102,187);
+
+    public static final Color GREY = new Color(136,136,136);
+
+    public static final Color PINK = new Color(187,0,102);
+
+}
