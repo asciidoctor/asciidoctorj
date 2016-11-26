dcd97885ba29f1f658a384a9873c4497006f4298
Robert Panzer <robert.panzer@me.com>
2016-11-26 16:49:12 +0100

    Ensure that content model and substitutions can be just copied from an existing node.

---
+++ asciidoctorj-core/src/test/groovy/org/asciidoctor/extension/WhenABlockShouldBeDuplicated.groovy
@@ -0,0 +1,93 @@
+package org.asciidoctor.extension
+
+import org.asciidoctor.Asciidoctor
+import org.asciidoctor.OptionsBuilder
+import org.asciidoctor.SafeMode
+import org.asciidoctor.ast.ContentModel
+import org.asciidoctor.ast.Document
+import org.jboss.arquillian.spock.ArquillianSputnik
+import org.jboss.arquillian.test.api.ArquillianResource
+import org.jsoup.Jsoup
+import org.junit.runner.RunWith
+import spock.lang.Specification
+
+@RunWith(ArquillianSputnik)
+class WhenABlockShouldBeDuplicated extends Specification {
+
+
+    public static final String VERBATIM = 'verbatim'
+    public static final String CLASS_LISTINGBLOCK = '.listingblock'
+    @ArquillianResource
+    private Asciidoctor asciidoctor
+
+    def 'it should be possible to copy the content_model'() {
+
+        given:
+        String newSource = 'a source'
+        asciidoctor.javaExtensionRegistry().treeprocessor(new BlockDuplicator('paragraph', newSource))
+
+        final String asciidoctorSource = '''
+
+  This will be ignored
+
+    '''
+
+        when:
+        Document document = asciidoctor.load(asciidoctorSource, OptionsBuilder.options().safe(SafeMode.SAFE).asMap())
+
+        then:
+        document.blocks.size() == 2
+        document.blocks[0].contentModel == VERBATIM
+        document.blocks[1].contentModel == VERBATIM
+    }
+
+
+
+    def 'it should be possible to copy substitutions'() {
+
+        given:
+        String newSource = 'Asciidoctor asciidoctor = ...'
+        asciidoctor.javaExtensionRegistry().treeprocessor(new BlockDuplicator('listing', newSource))
+
+        final String asciidoctorSource = '''
+[source,java,subs="replacements"]
+----
+This will be ignored
+----
+
+    '''
+
+        when:
+        org.jsoup.nodes.Document html = Jsoup.parse(asciidoctor.convert(asciidoctorSource, OptionsBuilder.options().safe(SafeMode.SAFE).headerFooter(false).asMap()))
+
+        then:
+        html.select(CLASS_LISTINGBLOCK).get(0).text() == 'This will be ignored'
+        html.select(CLASS_LISTINGBLOCK).get(1).text() == 'Asciidoctor asciidoctor = \u2026\u200B'
+    }
+
+    static class BlockDuplicator extends Treeprocessor {
+
+        private final String newSource
+        private final String newContext
+
+        BlockDuplicator(String newContext, String newSource) {
+            super(new HashMap<>())
+            this.newContext = newContext
+            this.newSource = newSource
+        }
+
+        @Override
+        Document process(Document document) {
+
+            String contentModel = document.blocks[0].contentModel
+            Map<Object, Object> options = new HashMap<Object, Object>()
+            options.put('subs', document.blocks[0].substitutions)
+            options.put(ContentModel.KEY, contentModel)
+
+            document.blocks << createBlock(document, newContext, newSource, [:], options)
+
+            document
+        }
+    }
+
+}
