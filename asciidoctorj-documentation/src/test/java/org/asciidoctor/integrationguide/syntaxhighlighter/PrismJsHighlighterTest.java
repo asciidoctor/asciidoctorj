97004c5d9aabc6d26afe8f7c1dbf0d8ec44130c7
Robert Panzer <robert.panzer@me.com>
2019-06-16 19:38:58 +0200

    Added documentation for static syntax highlighting.

---
+++ asciidoctorj-documentation/src/test/java/org/asciidoctor/integrationguide/syntaxhighlighter/PrismJsHighlighterTest.java
@@ -0,0 +1,78 @@
+package org.asciidoctor.integrationguide.syntaxhighlighter;
+
+import org.apache.commons.io.IOUtils;
+import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.AttributesBuilder;
+import org.asciidoctor.OptionsBuilder;
+import org.asciidoctor.SafeMode;
+import org.asciidoctor.util.ClasspathResources;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.FileReader;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Arquillian.class)
+public class PrismJsHighlighterTest {
+
+    @ArquillianResource
+    private Asciidoctor asciidoctor;
+
+    @ArquillianResource
+    private ClasspathResources classpathResources;
+
+    @Rule
+    public TemporaryFolder tempDir = new TemporaryFolder();
+
+    @Test
+    public void should_invoke_syntax_highlighter() throws Exception {
+//tag::include[]
+        File sources_adoc = //...
+//end::include[]
+            classpathResources.getResource("sources.adoc");
+
+//tag::include[]
+        File toDir = // ...
+//end::include[]
+            tempDir.newFolder();
+//tag::include[]
+
+        //tag::include[]
+        asciidoctor.syntaxHighlighterRegistry()
+            .register(PrismJsHighlighter.class, "prismjs"); // <1>
+
+        asciidoctor.convertFile(sources_adoc,
+            OptionsBuilder.options()
+                .headerFooter(true)
+                .toDir(toDir)
+                .safe(SafeMode.UNSAFE)
+                .attributes(AttributesBuilder.attributes()
+                    .sourceHighlighter("prismjs")           // <1>
+                    .copyCss(true)
+                    .linkCss(true)));
+
+        File docFile = new File(toDir, "sources.html");
+
+        Document document = Jsoup.parse(new File(toDir, "sources.html"), "UTF-8");
+        Elements keywords = document.select("div.content pre.highlight code span.token.keyword"); // <2>
+        assertThat(keywords, not(empty()));
+        assertThat(keywords.first().text(), is("public"));
+//end::include[]
+    }
+
+}
