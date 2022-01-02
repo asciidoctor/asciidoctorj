18ff7938e8f9914fb064d4f3720ffe8ddde7d819
Robert Panzer <robert.panzer.pb@googlemail.com>
2022-01-02 18:51:36 +0100

    Use symlinks to make tests available to docs instead of copying

--- docs/modules/ROOT/examples/org/asciidoctor/integrationguide/converter/TextConverterRegistry.java
+++ docs/modules/ROOT/examples/org/asciidoctor/integrationguide/converter/TextConverterRegistry.java
@@ -1,13 +1,1 @@
-package org.asciidoctor.integrationguide.converter;
-
-import org.asciidoctor.Asciidoctor;
-import org.asciidoctor.jruby.converter.spi.ConverterRegistry;
-
-public class TextConverterRegistry implements ConverterRegistry {
-    @Override
-    public void register(Asciidoctor asciidoctor) {
-
-        asciidoctor.javaConverterRegistry().register(TextConverter.class);
-
-    }
-}
+../../../../../../../../asciidoctorj-documentation/src/test/java/org/asciidoctor/integrationguide/converter/TextConverterRegistry.java
