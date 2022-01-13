6296455a86f7014b63e539d04548efbe09d30988
Gerald Combs <gerald@wireshark.org>
2022-01-12 18:40:53 -0800

    Chocolatey: Upgrade packaging to 2.5.3.

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@
 
 $ErrorActionPreference = 'Stop'; # stop on all errors
 
-$adocjVersion = '2.5.1'
+$adocjVersion = '2.5.3'
 $packageName  = 'asciidoctorj' # arbitrary name for the package, used in messages
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = "https://search.maven.org/remotecontent?filepath=org/asciidoctor/asciidoctorj/$($adocjVersion)/asciidoctorj-$($adocjVersion)-bin.zip"
@@ -16,8 +16,8 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   url           = $url
-  checksum      = '9fcf2fef6023257aaac21a3b5d8379c080019b1a408e43419f61d507ecddc95b'
-  checksumType  = 'sha256' #default is md5, can also be sha1
+  checksum      = '2af46345b0fc56fe9936a719f29185d12e89b8f0f2f85a3423da2e6a9448ded0'
+  checksumType  = 'sha256'
   #checksum64    = ''
   #checksumType64= 'md5' #default is checksumType
 }
