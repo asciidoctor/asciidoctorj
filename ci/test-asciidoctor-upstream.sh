309fa58091c2f3de9335c19da00c96b64919d8a9
Abel Salgado Romero <abelromero@gmail.com>
2023-04-15 21:09:32 +0200

    Fix CI upstream tests
    * Bump jruby used for gem installation to 9.2.21.0
    * Use s4u/setup-maven-action@v1.7.0
    * test-asciidoctor-upstream formatting

--- ci/test-asciidoctor-upstream.sh
+++ ci/test-asciidoctor-upstream.sh
@@ -20,15 +20,18 @@ wget --quiet -O $SRC_DIR.zip https://github.com/asciidoctor/asciidoctor/archive/
 unzip -q $SRC_DIR.zip
 cp ../../ci/asciidoctor-gem-installer.pom $SRC_DIR/pom.xml
 cd $SRC_DIR
-readonly ASCIIDOCTOR_VERSION=`grep 'VERSION' ./lib/asciidoctor/version.rb | sed "s/.*'\(.*\)'.*/\1/" | sed "s/[.]dev$/.dev-SNAPSHOT/"`
+readonly ASCIIDOCTOR_VERSION="$(grep 'VERSION' ./lib/asciidoctor/version.rb | sed "s/.*'\(.*\)'.*/\1/" | sed "s/[.]dev$/.dev-SNAPSHOT/")-SNAPSHOT"
 # we don't use sed -i here for compatibility with OSX
 sed "s;<version></version>;<version>$ASCIIDOCTOR_VERSION</version>;" pom.xml > pom.xml.sedtmp && \
   mv -f pom.xml.sedtmp pom.xml
 
-# we override the jruby version here with one supported by java9, additionally java9 needs some add-opens arguments that need to be ignored on older jvms
-mvn install -B --no-transfer-progress -Dgemspec=asciidoctor.gemspec -Djruby.version=9.2.17.0 -Djruby.jvmargs="-XX:+IgnoreUnrecognizedVMOptions --add-opens=java.base/java.security.cert=ALL-UNNAMED --add-opens=java.base/java.security=ALL-UNNAMED --add-opens=java.base/java.util.zip=ALL-UNNAMED"
+# we override the jruby version here with latest j8 supported, additionally java9 needs some add-opens arguments that need to be ignored on older jvms
+mvn install -B --no-transfer-progress \
+  -Dgemspec="asciidoctor.gemspec" \
+  -Djruby.version="9.2.21.0" \
+  -Djruby.jvmargs="-XX:+IgnoreUnrecognizedVMOptions --add-opens=java.base/java.security.cert=ALL-UNNAMED --add-opens=java.base/java.security=ALL-UNNAMED --add-opens=java.base/java.util.zip=ALL-UNNAMED"
 popd
 
 ## Test against installed gem
-./gradlew -S -Pskip.signing -PasciidoctorGemVersion=$ASCIIDOCTOR_VERSION -PuseMavenLocal=true check
+./gradlew -S -Pskip.signing -PasciidoctorGemVersion="$ASCIIDOCTOR_VERSION" -PuseMavenLocal=true check
 exit $?
