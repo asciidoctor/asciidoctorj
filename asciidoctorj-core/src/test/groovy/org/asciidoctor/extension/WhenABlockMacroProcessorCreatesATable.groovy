fb66c12e02e98f3021c3de961ee4d3f1121d13cf
Robert Panzer <Robert.Panzer.PB@googlemail.com>
2024-04-07 15:48:38 +0200

    Support column widths 2.5.x (#1271)
    * Fixes #1265. Support setting column widths
    
    * Update changelog

--- asciidoctorj-core/src/test/groovy/org/asciidoctor/extension/WhenABlockMacroProcessorCreatesATable.groovy
+++ asciidoctorj-core/src/test/groovy/org/asciidoctor/extension/WhenABlockMacroProcessorCreatesATable.groovy
@@ -1,6 +1,7 @@
 package org.asciidoctor.extension
 
 import org.asciidoctor.Asciidoctor
+import org.asciidoctor.Options
 import org.asciidoctor.OptionsBuilder
 import org.asciidoctor.SafeMode
 import org.asciidoctor.util.ClasspathResources
@@ -9,9 +10,12 @@ import org.jboss.arquillian.spock.ArquillianSputnik
 import org.jboss.arquillian.test.api.ArquillianResource
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 import org.junit.rules.TemporaryFolder
 import org.junit.runner.RunWith
 import spock.lang.Specification
+
+import static java.nio.charset.StandardCharsets.UTF_8
 
 @RunWith(ArquillianSputnik)
 class WhenABlockMacroProcessorCreatesATable extends Specification {
@@ -20,10 +24,23 @@ class WhenABlockMacroProcessorCreatesATable extends Specification {
     public static final String SECOND_TD = 'td:nth-child(2)'
     public static final String THIRD_TD  = 'td:nth-child(3)'
     public static final String IMG_ELEMENT  = 'img'
+    public static final String COL  = 'col'
+    public static final String STYLE  = 'style'
+    public static final String WIDTH  = 'width'
+    public static final String WIDTH_2  = '2%'
+    public static final String WIDTH_3  = '3%'
+    public static final String WIDTH_20  = '20%'
+    public static final String WIDTH_40  = '40%'
     public static final String CONTRIBUTOR = 'bob'
     public static final String BLOCKMACRO_NAME = 'githubcontributors'
 
     public static final String AVATAR_URL_REGEXP = /https:\/\/avatars.githubusercontent.com\/u\/.*/
+    public static final String CSS_QUERY_TABLE = 'table'
+    public static final String CLASS_GRID_ROWS = 'grid-rows'
+    public static final String CLASS_HALIGN_LEFT = 'halign-left'
+    public static final String CLASS_HALIGN_CENTER = 'halign-center'
+    public static final String ATTR_SRC = 'src'
+    public static final int THREE = 3
 
     @ArquillianResource
     private Asciidoctor asciidoctor
@@ -31,14 +48,21 @@ class WhenABlockMacroProcessorCreatesATable extends Specification {
     @ArquillianResource
     private TemporaryFolder tmp
 
+    @ArquillianResource
+    private ClasspathResources classpathResources
+
     private static final String DOCUMENT = '''
 = AsciidoctorJRuby contributors
 
 githubcontributors::asciidoctor/asciidoctorj[]
 '''
 
-    @ArquillianResource
-    private ClasspathResources classpathResources
+    private static final String DOCUMENT_WITH_NEGATIVE_WIDTHS = '''
+= AsciidoctorJRuby contributors
+
+githubcontributors::asciidoctor/asciidoctorj[widths="2,3,-1"]
+'''
+
 
     def setup() {
         TestHttpServer.start(['http://api.github.com/repos/asciidoctor/asciidoctorj/contributors' : classpathResources.getResource('githubcontributors.json')])
@@ -60,18 +84,61 @@ githubcontributors::asciidoctor/asciidoctorj[]
         asciidoctor.convert(DOCUMENT, OptionsBuilder.options().safe(SafeMode.SAFE).inPlace(false).baseDir(tmp.getRoot()).toDir(tmp.getRoot()).toFile(resultFile))
 
         then:
-        Document htmlDocument = Jsoup.parse(resultFile, 'UTF-8')
+        Document htmlDocument = Jsoup.parse(resultFile, UTF_8.name())
 
-        htmlDocument.select('table').hasClass('grid-rows')
+        Elements cols = htmlDocument.select(COL)
+        cols.size() == THREE
+        cols.get(0).attr(STYLE).contains(WIDTH_20) || cols.get(0).attr(WIDTH).equals(WIDTH_20)
+        cols.get(1).attr(STYLE).contains(WIDTH_40) || cols.get(1).attr(WIDTH).equals(WIDTH_40)
+        cols.get(2).attr(STYLE).contains(WIDTH_40) || cols.get(2).attr(WIDTH).equals(WIDTH_40)
+
+        htmlDocument.select(CSS_QUERY_TABLE).hasClass(CLASS_GRID_ROWS)
 
         htmlDocument.select(FIRST_TD).every { tdElement -> tdElement.select(IMG_ELEMENT).size() == 1 }
-        htmlDocument.select(FIRST_TD).every { tdElement -> tdElement.select(IMG_ELEMENT)[0].attr('src') =~ AVATAR_URL_REGEXP }
+        htmlDocument.select(FIRST_TD).every { tdElement -> tdElement.select(IMG_ELEMENT)[0].attr(ATTR_SRC) =~ AVATAR_URL_REGEXP }
 
         htmlDocument.select(SECOND_TD).size() == htmlDocument.select(SECOND_TD) != 0
         htmlDocument.select(SECOND_TD).size() != 0
 
-        htmlDocument.select(SECOND_TD).every { tdElement -> tdElement.hasClass('halign-left')}
-        htmlDocument.select(THIRD_TD).every { tdElement -> tdElement.hasClass('halign-center')}
+        htmlDocument.select(SECOND_TD).every { tdElement -> tdElement.hasClass(CLASS_HALIGN_LEFT) }
+        htmlDocument.select(THIRD_TD).every { tdElement -> tdElement.hasClass(CLASS_HALIGN_CENTER) }
+
+        htmlDocument.select(SECOND_TD).find { tdElement -> tdElement.text() == CONTRIBUTOR } != null
+    }
+
+    def "the table should be rendered to html with negative widths"() {
+        given:
+        asciidoctor.javaExtensionRegistry().blockMacro(BLOCKMACRO_NAME, GithubContributorsBlockMacro)
+        File resultFile = tmp.newFile('resultWithNegativeWidth.html')
+
+        when:
+        def options = Options.builder()
+                .safe(SafeMode.SAFE)
+                .inPlace(false)
+                .baseDir(tmp.getRoot())
+                .toFile(resultFile)
+                .build()
+        asciidoctor.convert(DOCUMENT_WITH_NEGATIVE_WIDTHS, options)
+
+        then:
+        Document htmlDocument = Jsoup.parse(resultFile, UTF_8.name())
+
+        Elements cols = htmlDocument.select(COL)
+        cols.size() == THREE
+        cols.get(0).attr(STYLE).contains(WIDTH_2) || cols.get(0).attr(WIDTH).equals(WIDTH_2)
+        cols.get(1).attr(STYLE).contains(WIDTH_3) || cols.get(1).attr(WIDTH).equals(WIDTH_3)
+        cols.get(2).attr(STYLE).length() == 0 && cols.get(2).attr(WIDTH).length() == 0
+
+        htmlDocument.select(CSS_QUERY_TABLE).hasClass(CLASS_GRID_ROWS)
+
+        htmlDocument.select(FIRST_TD).every { tdElement -> tdElement.select(IMG_ELEMENT).size() == 1 }
+        htmlDocument.select(FIRST_TD).every { tdElement -> tdElement.select(IMG_ELEMENT)[0].attr(ATTR_SRC) =~ AVATAR_URL_REGEXP }
+
+        htmlDocument.select(SECOND_TD).size() == htmlDocument.select(SECOND_TD) != 0
+        htmlDocument.select(SECOND_TD).size() != 0
+
+        htmlDocument.select(SECOND_TD).every { tdElement -> tdElement.hasClass(CLASS_HALIGN_LEFT) }
+        htmlDocument.select(THIRD_TD).every { tdElement -> tdElement.hasClass(CLASS_HALIGN_CENTER) }
 
         htmlDocument.select(SECOND_TD).find { tdElement -> tdElement.text() == CONTRIBUTOR } != null
     }
